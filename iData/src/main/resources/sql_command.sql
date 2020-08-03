c87c6c41dc30d84ed2a864b4d8283232ab6976d2
Thomas Leung <thomas060142@outlook.com>
2020-08-03 18:51:42 +0800

    add hibernate

---
+++ iData/src/main/resources/sql_command.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `temperature` modify `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+insert into temperature (temperature, elder_id) value(33, 2) 
+mysql alter table auto_increment = 1
